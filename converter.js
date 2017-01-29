e5df54b176f63129b6fce77f61adbab324e1373b
webbdm <gwebb762@gmail.com>
2017-01-28 23:59:49 -0600

    added buttons

---
+++ converter.js
@@ -0,0 +1,19 @@
+function toCelsius () {
+
+}
+
+function toFahrenheit () {
+
+}
+
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+function determineConverter (clickEvent) {
+  console.log("event", clickEvent);
+}
+
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter);
