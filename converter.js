fb47d3098f87a42320f6cd2dc2ab4c442f480a8d
webbdm <gwebb762@gmail.com>
2017-01-29 21:04:40 -0600

    added variables

--- converter.js
+++ converter.js
@@ -1,13 +1,23 @@
+inputNum = 0;
+
+var outputTemp= 0;
+
+
 function toCelsius () {
+
+outputTemp = (5/9) * (inputNum - 32);
+
 
 }
 
 function toFahrenheit () {
 
+outputTemp (9/5) * (inputNum + 32);
+
 }
 
 // Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
+var button = document.getElementById("converTemp");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
@@ -17,3 +27,7 @@ function determineConverter (clickEvent) {
 
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
+
+// Loop that changes converTemp color
+
+if outputTemp = 
