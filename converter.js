9b198205ea9cd2613a3d1742fb3e5638fc70f693
webbdm <gwebb762@gmail.com>
2017-01-29 22:04:07 -0600

    added buttons

--- converter.js
+++ converter.js
@@ -1,23 +1,25 @@
 inputNum = 0;
+outputC = 0;
+outputF = 0;
 
-var outputTemp= 0;
 
 
 function toCelsius () {
 
-outputTemp = (5/9) * (inputNum - 32);
+	outputC = (5/9) * (inputNum - 32);
 
 
 }
 
 function toFahrenheit () {
 
-outputTemp (9/5) * (inputNum + 32);
+	outputF = (9/5) * (inputNum + 32);
 
 }
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converTemp");
+// Get a reference to the button element in the DOM and the converter DIV
+var button = document.getElementById("convert");
+var element = document.getElementById("converTemp");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
@@ -28,6 +30,19 @@ function determineConverter (clickEvent) {
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
 
-// Loop that changes converTemp color
+// Conditionals that changes converTemp color
 
-if outputTemp = 
+if (outputC > 32 || outputF > 90 ) {
+
+	//change text color to red
+
+} else if (outputC < 0 || outputF < 32 ) {
+
+	//change text color to blue
+
+} else {
+
+	//change text color to green
+
+}
+
