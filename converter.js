096185bfd56e66a22cf660a452a8fda82a65084a
webbdm <gwebb762@gmail.com>
2017-01-30 18:43:35 -0600

    reorganized the functions and variables

--- converter.js
+++ converter.js
@@ -1,37 +1,54 @@
-inputNum = 0;
-outputC = 0;
-outputF = 0;
+var inputNum = 74; //document.getElementById("initialTemp").value;
+console.log(inputNum);
 
+function toCelsius (temp) {
 
-
-function toCelsius () {
-
-	outputC = (5/9) * (inputNum - 32);
-
-	return outputC
-
+	document.getElementById("convertedTemp").innerHTML = (temp - 32) * (5/9);
+  var newCelTemp = document.getElementById("convertedTemp").innerHTML;
+  console.log("Converted to Cel temp", newCelTemp, inputNum);
+  
 }
 
-function toFahrenheit () {
+function toFahrenheit (temp) {
 
-	outputF = (9/5) * (inputNum + 32);
-
-	return outputF
-
+	document.getElementById("convertedTemp").innerHTML = temp * (9/5) + 32;
+  var newFahTemp = document.getElementById("convertedTemp").innerHTML;
+  console.log("Converted to Fah temp", newFahTemp, inputNum);
+  
 }
+
+
+// toCelsius(inputNum);
+// toFahrenheit(inputNum);
 
 // Get a reference to the button element in the DOM and the converter DIV
-var button = document.getElementById("convert");
-var element = document.getElementById("converTemp");
+var button = document.getElementById("converter");
+var element = document.getElementById("convertedTemp");
+
+
+radio[0].addEventListener("click", function(clickEvent) {
+    var checkedButton = clickEvent.target.value;
+  });
+
+radio[1].addEventListener("click", function(clickEvent) {
+    var checkedButton = clickEvent.target.value;
+  });
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
   
-	if 
-
-
-
+  if (checkedButton === "celsius"){
+    
+      toFahrenheit(inputNum);
+  }
+  
+  if (checkedButton === "fahrenheit"){
+    
+      toCelsius(inputNum);   
+    
+  }
+  
   console.log("event", clickEvent);
 }
 
@@ -40,11 +57,11 @@ button.addEventListener("click", determineConverter);
 
 // Conditionals that changes converTemp color
 
-if (outputC > 32 || outputF > 90 ) {
+if (newCelTemp > 32 || newFahTemp > 90 ) {
 
 	//change text color to red
 
-} else if (outputC < 0 || outputF < 32 ) {
+} else if (newCelTemp < 0 || newFahTemp < 32 ) {
 
 	//change text color to blue
 
@@ -53,4 +70,3 @@ if (outputC > 32 || outputF > 90 ) {
 	//change text color to green
 
 }
-
